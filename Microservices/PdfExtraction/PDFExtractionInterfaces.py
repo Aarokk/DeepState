bea15f3b4e2664823e21a81ea8b99bb7e70cb6f7
Aroka <arocapu36@gmail.com>
2025-03-18 11:46:26 +0100

    update: defined and documented PDF extraction interfaces

---
+++ Microservices/PdfExtraction/PDFExtractionInterfaces.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+
+class PDFExtractionInterfaces(ABC):
+
+    """
+        The class that implements the following methods should receive the following parameters from the GUI:
+        search_fields_content[] -> Array composed of four strings
+        sections_content[] -> Array composed of six booleans
+        logic_selectors_content[] -> Array composed of four strings representing the logic operators
+        date_content[] -> Array composed of two strings representing the start and end dates
+        date_content -> String representing the number of results per page desired
+        date_order_by -> String representing the order desired by date
+    """
+
+    @abstractmethod
+    def create_temp_folder(self):
+        """
+             Creates a temporary folder to store the extracted PDFs
+             the folder must be failsafe, if an exception occurs during extraction
+             e.g. BOE not online, search without results, etc., the exception will
+             call cleanup()
+        """
+        pass
+
+    @abstractmethod
+    def extract(self):
+        """
+            receives parameters from GUI and extracts PDFs from BOE based on them
+            if the search fails, the exception will call cleanup(),
+            should send some type of signal to the GUI when extraction finishes
+        """
+        pass
+
+    @abstractmethod
+    def cleanup(self):
+        """
+            deletes the temporary folder created by create_temp_folder() and all its contents
+        """
+        pass
+
+    @abstractmethod
+    def write_search_params(self):
+        """
+            writes the search parameters to a json file in the temporary folder
+            must check if the folder exists first
+            should be called after extract(), if the search succeeds
+        """
+        pass
---
+++ Microservices/PdfExtraction/__init__.py
@@ -0,0 +1,5 @@
+from .PDFExtractionInterfaces import PDFExtractionInterfaces
+from .PDFExtractionService import PDFExtractionService
+
+# Define accessible files
+__all__ = ["PDFExtractionService", "PDFExtractionInterfaces"]
