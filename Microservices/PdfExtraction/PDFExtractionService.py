f85a4acb5a5325292733b062e66d11c8555068bb
Aroka <arocapu36@gmail.com>
2025-03-18 12:21:48 +0100

    update: implemented first two abstract methods and builder

--- Microservices/PdfExtraction/PDFExtractionService.py
+++ Microservices/PdfExtraction/PDFExtractionService.py
@@ -1,16 +1,37 @@
 import logging
 import os
 import shutil
+from typing import List
 
 from Microservices.PdfExtraction import PDFExtractionInterfaces
 
+# Logger setup
 logging.basicConfig(level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler())
 
+# Define a route for temporal folder (this one is for ease of testing)
+# TO-DO: Establish a new route within the application files
 TEMP_FOLDER_ROUTE = "C:\\temporal_pdf_files"
 
 
 class PDFExtractionService(PDFExtractionInterfaces):
+    # Class builder receives the parameters specified in PDFExtractionInterfaces
+    # Information about expected contents is to be found inside the class
+    def __init__(self,
+                 search_fields_content: List[str],
+                 sections_content: List[bool],
+                 logic_selectors_content: List[str],
+                 date_content: List[str],
+                 results_per_page: str,
+                 date_order_by: str
+                 ):
+        self.search_fields_content = search_fields_content
+        self.sections_content = sections_content
+        self.logic_selectors_content = logic_selectors_content
+        self.date_content = date_content
+        self.results_per_page = results_per_page
+        self.date_order_by = date_order_by
+
     # Implementation of an abstract method
     def create_temp_folder(self):
         try:
